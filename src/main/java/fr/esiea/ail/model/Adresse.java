b4ecdf5173963301809241a1995cb5ee89fb6358
hamide-samir <hamide.samir@gmail.com>
2013-11-04 13:35:27 +0100

    second commit

--- src/main/java/fr/esiea/ail/model/Adresse.java
+++ src/main/java/fr/esiea/ail/model/Adresse.java
@@ -5,39 +5,58 @@ public class Adresse {
 	private String alias;
 	private String numero;
 	private String rue;
-	private String codepostale;
+	private String codepostal;
 	private String ville;
+	private Integer id;
 	
 	public String getNumero() {
 		return numero;
 	}
+	
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
+	
 	public String getRue() {
 		return rue;
 	}
+	
 	public void setRue(String rue) {
 		this.rue = rue;
 	}
-	public String getCodepostale() {
-		return codepostale;
+	
+	public String getCodepostal() {
+		return codepostal;
 	}
-	public void setCodepostale(String codepostale) {
-		this.codepostale = codepostale;
+	
+	public void setCodepostal(String codepostal) {
+		this.codepostal = codepostal;
 	}
+	
 	public String getVille() {
 		return ville;
 	}
+	
 	public void setVille(String ville) {
 		this.ville = ville;
 	}
+	
 	public String getAlias() {
 		return alias;
 	}
+	
 	public void setAlias(String alias) {
 		this.alias = alias;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
 	
 	
 }
